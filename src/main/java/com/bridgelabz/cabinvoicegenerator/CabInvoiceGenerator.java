68206538434fe872d2ee1e901fc407d89852f82b
unknown <divyacharaninabha2gmail.com>
2020-10-21 01:22:11 +0530

    Added: Welcome message added

---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class CabInvoiceGenerator {
+
+	private static final Logger LOG = LogManager.getLogger(CabInvoiceGenerator.class);
+
+	public static void main(String[] args) {
+
+		//Welcome Message
+		LOG.info("Welcome to Cab Invoice Generator Program");
+	}
+
+}
